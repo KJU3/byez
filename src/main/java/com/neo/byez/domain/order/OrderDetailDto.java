ec687efff887e30d52ca8d368cf6ca1c0b697436
AlishaHong <ugyung87@gmail.com>
2024-05-17 08:55:21 +0900

    [test] 주문 파트 Dao 2차 테스트
    Dao(ItemOptionDao, OrdEtcReqDao)테스트 완료

--- src/main/java/com/neo/byez/domain/order/OrderDetailDto.java
+++ src/main/java/com/neo/byez/domain/order/OrderDetailDto.java
@@ -42,6 +42,23 @@ public class OrderDetailDto {
         this.ord_state = ord_state;
     }
 
+    public OrderDetailDto(){
+
+    }
+
+    public OrderDetailDto(String ord_num, String item_num, String id, String item_name, Integer price, Integer item_qty, Integer item_price, String opt1, String opt2, String ord_state) {
+        this.ord_num = ord_num;
+        this.item_num = item_num;
+        this.id = id;
+        this.item_name = item_name;
+        this.price = price;
+        this.item_qty = item_qty;
+        this.item_price = item_price;
+        this.opt1 = opt1;
+        this.opt2 = opt2;
+        this.ord_state = ord_state;
+    }
+
     public String getOrd_num() {
         return ord_num;
     }
